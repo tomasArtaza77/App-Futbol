0122bc9132592b11dcf0026ff66b9bb3ef885e04
tomasArtaza77 <tomasartaza25@gmail.com>
2021-03-05 16:16:32 -0300

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BocaComponent } from './boca/boca.component';
+import { RiverComponent } from './river/river.component';
+import { BackgroundComponent } from './background/background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BocaComponent,
+    RiverComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/river/river.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-river',
+  templateUrl: './river.component.html',
+  styleUrls: ['./river.component.css']
+})
+export class RiverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
